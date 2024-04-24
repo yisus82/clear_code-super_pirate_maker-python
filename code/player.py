9cfd32da703c627f46b33a0b4429935b1e3b3cc1
yisus82 <djalma8@gmail.com>
2024-04-24 03:29:38 +0200

    Fix collision bug

--- code/player.py
+++ code/player.py
@@ -1,5 +1,11 @@
 import pygame
-from settings import GRAVITY, JUMP_FORCE, PLAYER_HITBOX_OFFSET, PLAYER_SPEED
+from settings import (
+    COLLISION_OFFSET,
+    GRAVITY,
+    JUMP_FORCE,
+    PLAYER_HITBOX_OFFSET,
+    PLAYER_SPEED,
+)
 from sprites import Animated
 
 
@@ -60,53 +66,77 @@ class Player(Animated):
         self.direction.y += GRAVITY * dt
 
     def check_on_floor(self):
-        floor_rect = pygame.Rect(self.hitbox.bottomleft, (self.hitbox.width, 10))
+        self.on_floor = self.sprite_down_collide() is not None
+
+    def sprite_left_collide(self):
+        left_rect = pygame.Rect(
+            self.hitbox.topleft, (COLLISION_OFFSET, self.hitbox.height)
+        )
         for sprite in self.collision_sprites:
-            sprite_rect = pygame.Rect(sprite.hitbox.topleft, (sprite.hitbox.width, 10))
-            if sprite_rect.colliderect(floor_rect):
-                self.on_floor = True
-                self.hitbox.bottom = sprite.hitbox.top
-                self.direction.y = 0
-                return
-        self.on_floor = False
+            sprite_rect = pygame.Rect(
+                sprite.hitbox.topright, (COLLISION_OFFSET, sprite.hitbox.height)
+            )
+            if sprite_rect.colliderect(left_rect):
+                return sprite
+        return None
+
+    def sprite_right_collide(self):
+        right_rect = pygame.Rect(
+            self.hitbox.topright, (COLLISION_OFFSET, self.hitbox.height)
+        )
+        for sprite in self.collision_sprites:
+            sprite_rect = pygame.Rect(
+                sprite.hitbox.topleft, (COLLISION_OFFSET, sprite.hitbox.height)
+            )
+            if sprite_rect.colliderect(right_rect):
+                return sprite
+        return None
+
+    def sprite_up_collide(self):
+        up_rect = pygame.Rect(
+            self.hitbox.topleft, (self.hitbox.width, COLLISION_OFFSET * 2)
+        )
+        for sprite in self.collision_sprites:
+            sprite_rect = pygame.Rect(
+                sprite.hitbox.bottomleft, (sprite.hitbox.width, COLLISION_OFFSET)
+            )
+            if sprite_rect.colliderect(up_rect):
+                return sprite
+        return None
+
+    def sprite_down_collide(self):
+        down_rect = pygame.Rect(
+            self.hitbox.bottomleft, (self.hitbox.width, COLLISION_OFFSET)
+        )
+        for sprite in self.collision_sprites:
+            sprite_rect = pygame.Rect(
+                sprite.hitbox.topleft, (sprite.hitbox.width, COLLISION_OFFSET)
+            )
+            if sprite_rect.colliderect(down_rect):
+                return sprite
+        return None
 
     def horizontal_collide(self):
         if self.direction.x > 0:  # moving right
-            right_rect = pygame.Rect(self.hitbox.topright, (10, self.hitbox.height))
-            for sprite in self.collision_sprites:
-                sprite_rect = pygame.Rect(
-                    sprite.hitbox.topleft, (10, sprite.hitbox.height)
-                )
-                if sprite_rect.colliderect(right_rect):
-                    self.hitbox.right = sprite.hitbox.left
+            sprite = self.sprite_right_collide()
+            if sprite:
+                self.hitbox.right = sprite.hitbox.left
         elif self.direction.x < 0:  # moving left
-            left_rect = pygame.Rect(self.hitbox.topleft, (10, self.hitbox.height))
-            for sprite in self.collision_sprites:
-                sprite_rect = pygame.Rect(
-                    sprite.hitbox.topright, (10, sprite.hitbox.height)
-                )
-                if sprite_rect.colliderect(left_rect):
-                    self.hitbox.left = sprite.hitbox.right
+            sprite = self.sprite_left_collide()
+            if sprite:
+                self.hitbox.left = sprite.hitbox.right
 
     def vertical_collide(self):
         if self.direction.y > 0:  # moving down
-            bottom_rect = pygame.Rect(self.hitbox.bottomleft, (self.hitbox.width, 10))
-            for sprite in self.collision_sprites:
-                sprite_rect = pygame.Rect(
-                    sprite.hitbox.topleft, (sprite.hitbox.width, 10)
-                )
-                if sprite_rect.colliderect(bottom_rect):
-                    self.hitbox.bottom = sprite.hitbox.top
-                    self.direction.y = 0
+            sprite = self.sprite_down_collide()
+            if sprite:
+                self.hitbox.bottom = sprite.hitbox.top
+                self.direction.y = 0
         elif self.direction.y < 0:  # moving up
-            top_rect = pygame.Rect(self.hitbox.topleft, (self.hitbox.width, 10))
-            for sprite in self.collision_sprites:
-                sprite_rect = pygame.Rect(
-                    sprite.hitbox.bottomleft, (sprite.hitbox.width, 10)
-                )
-                if sprite_rect.colliderect(top_rect):
-                    self.hitbox.top = sprite.hitbox.bottom
-                    self.direction.y = 0
+            sprite = self.sprite_up_collide()
+            if sprite:
+                self.hitbox.top = sprite.hitbox.bottom
+                self.direction.y = 0
 
     def update(self, dt):
         self.check_on_floor()
